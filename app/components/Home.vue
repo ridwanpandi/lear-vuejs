1d4f926b5083afc55c899178c4946e6c72467946
Ridwan <ridwanpandi94@gmail.com>
2018-09-25 11:42:13 +0700

    add home page component

---
+++ app/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <b-jumbotron  v-bind:header="getTitle" lead="Belajar Vue JS" >
+        <b-btn variant="primary" href="javascript:changeTitle('Title Dari Vuex')"  >Ubah Judul</b-btn>
+        </b-jumbotron>
+        <button @click="changeTitle('Judul diubah')">Ubah Judul</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: this.$store.getters.getTitle,
+        }
+    },
+    computed: {
+        getTitle(){
+            return  this.$store.getters.getTitle
+        }
+    },
+    methods: {
+        changeTitle: function (newTitle) {
+            console.log(newTitle);
+            this.$store.dispatch("changeTitle", newTitle);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
