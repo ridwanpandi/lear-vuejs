01dff46fb5fa7e657f846dd2bf311c59a70a1b40
Ridwan <ridwanpandi94@gmail.com>
2018-09-25 11:41:51 +0700

    add child components

---
+++ app/components/ChildComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>Numbers First Child Component : {{ numbers }}</p>
+        <p>ChildNumbers in First Child Component : {{ childnumbers }}</p>
+        <input type="text" v-model="number">
+        <button @click="$emit('add-number', Number(number))">Add Number</button>
+    </div>
+</template>
+
+<script>
+import {EventBus} from '../events/event-bus.js'
+
+export default {
+    name: "ChildComponent",
+    props:{
+        numbers: Array
+    },
+    data() {
+        return {
+            childnumbers: [3,2,1],
+            number: 0
+        }
+    },
+    created(){
+        EventBus.$on("add-number-childcomponent2", newNumber => {
+            this.childnumbers.push(newNumber)
+        })
+    },
+}
+</script>
+
---
+++ app/components/ChildComponent2.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button @click="addNumber()">add 9 value to numbers from ChildComponent2</button>
+    </div>
+</template>
+
+<script>
+import {EventBus} from '../events/event-bus.js'
+
+export default {
+    name: "ChildComponent2",
+    data() {
+        return {
+            number: 9
+        }
+    },
+    methods: {
+        addNumber(newNumber) {
+            // bisa get newNumber juga tapi kasih parameter di addNumber 
+            EventBus.$emit("add-number-childcomponent2", Number(this.number))
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
