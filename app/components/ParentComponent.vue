626f865a87b86ccf59ce1fd4fca77ba63e94faab
Ridwan <ridwanpandi94@gmail.com>
2018-09-25 11:42:27 +0700

    try to implement with parent component

---
+++ app/components/ParentComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        {{ numbers }}
+        <Child 
+        :numbers="numbers"
+        @add-number="numbers.push($event)"></Child>
+        <Child2></Child2>
+    </div>
+</template>
+
+<script>
+import Child from './ChildComponent.vue'
+import Child2 from './ChildComponent2.vue'
+
+export default {
+    data(){
+        return {
+            numbers: [1,2,3]
+        }
+    },
+    components: {
+        Child,
+        Child2
+    }
+}
+</script>
