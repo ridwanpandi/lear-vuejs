326db887b835aa28063c84eb1a59c03b32ac1cb3
Ridwan <ridwanpandi94@gmail.com>
2018-09-25 11:41:08 +0700

    add app vue

---
+++ app/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+
+        <div>
+            <b-nav>
+                <b-nav-item to="/">Home</b-nav-item>
+                <b-nav-item to="/login">Login</b-nav-item>
+                <b-nav-item to="/logout" v-if="isLoggedIn" @click="doLogout()">Logout</b-nav-item>
+            </b-nav>
+        </div>
+        
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import {logout} from './utils/auth.js'
+
+export default {
+    name: "App",
+    data(){
+        return {
+        }
+    },
+    computed: {
+        isLoggedIn: function () {
+            let token = this.$store.getters.getToken;
+            console.log(token);
+            if (token != null) {
+                return true
+            } else {
+                return false;
+            }
+        }
+    }, 
+    methods: {
+        doLogout() {
+            logout();
+            this.$router.go('/');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
